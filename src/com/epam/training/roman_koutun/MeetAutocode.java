cf9955641c725d4761558638d0d813d1255936b0
kovtunroman <9696465@gmail.com>
2023-01-09 22:00:35 +0300

    minor changes

--- src/com/epam/training/roman_koutun/MeetAutocode.java
+++ src/com/epam/training/roman_koutun/MeetAutocode.java
@@ -2,6 +2,6 @@ package com.epam.training.roman_koutun;
 
 public class MeetAutocode {
     public static void main(String[] args) {
-        System.out.print("Hello, Autocode!");
+        System.out.print("Hello,Autocode!");
     }
 }
